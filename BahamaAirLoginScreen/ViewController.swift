4f21fc245b653293eb66d7fe2a83266fb0983cad
travisgillespie <tgilles34@hotmail.com>
2016-01-06 13:37:49 -0600

    Transitions Banner Multiple Messages

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -36,6 +36,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     let label = UILabel()
     let messages = ["Connecting ...", "Authorizing ...", "Sending credentials ...", "Failed"]
     
+    var statusPosition = CGPoint.zero
+    
     // MARK view controller methods
     
     override func viewDidLoad() {
@@ -59,6 +61,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         label.textColor = UIColor(red: 0.89, green: 0.38, blue: 0.0, alpha: 1.0)
         label.textAlignment = .Center
         status.addSubview(label)
+        
+        statusPosition = status.center
         
     }
     
@@ -160,7 +164,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
             self.status.hidden = false
             }, completion: {_ in
                 //transition completion
+                delay(seconds: 0.2) {
+                    if index < self.messages.count-1{
+                        self.removeMessage(index: index)
+                    } else {
+                        //reset form
+                    }
+                }
                 
+        })
+    }
+    
+    func removeMessage(index index: Int){
+        UIView.animateWithDuration(0.33, delay: 0.0, options: [], animations:
+            {
+                self.status.center.x += self.view.frame.size.width
+            }, completion: {_ in
+                self.status.hidden = true
+                self.status.center = self.statusPosition
+                
+                self.showMessage(index: index+1)
         })
     }
     
